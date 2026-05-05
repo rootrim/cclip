18a4aa4210f9db493087331f439ffb5a16ab2161
trim <omuaz01@gmail.com>
2026-05-05 19:31:03 +0300

    xxHash -> xxhash

--- flake.nix
+++ flake.nix
@@ -3,28 +3,31 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      packages.${system} = {
-        cclip = pkgs.stdenv.mkDerivation {
-          pname = "cclip";
-          version = "3.3.1";
-          src = ./.;
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system} = {
+      cclip = pkgs.stdenv.mkDerivation {
+        pname = "cclip";
+        version = "3.3.1";
+        src = ./.;
 
-          nativeBuildInputs = with pkgs; [
-            meson
-            ninja
-            pkg-config
-            wayland-scanner
-            git
-          ];
+        nativeBuildInputs = with pkgs; [
+          meson
+          ninja
+          pkg-config
+          wayland-scanner
+          git
+        ];
 
-          buildInputs = with pkgs; [ wayland sqlite xxHash ];
-        };
-        default = self.packages.${system}.cclip;
+        buildInputs = with pkgs; [wayland sqlite xxhash];
       };
+      default = self.packages.${system}.cclip;
     };
+  };
 }
